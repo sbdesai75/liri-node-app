0f20676113a78011da80d9454e7551035da7be17
sbdesai75 <sbdesai75@yahoo.com>
2018-10-20 11:48:05 -0700

    updated files

---
+++ liri.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
